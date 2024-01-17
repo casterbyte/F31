9ac01ea9c9f6ad638f624365b5eff5e690fcae37
Caster <88490676+wearecaster@users.noreply.github.com>
2024-01-17 18:53:02 +0600

    Update F31.sh

--- F31.sh
+++ F31.sh
@@ -89,7 +89,7 @@ else
 fi
 
 # Increasing and shifting TTL value 
-echo -e "\n${YELLOW}[+] Increasing and shifting TTL (TTL=210)${NC}"
+echo -e "\n${YELLOW}[+] Increasing and shifting TTL (TTL=80)${NC}"
 if sudo sysctl -w net.ipv4.ip_default_ttl=80 > /dev/null 2>&1 && sudo iptables -t mangle -A PREROUTING -i "${INTERFACE}" -j TTL --ttl-inc 1 > /dev/null 2>&1; then
     echo -e "${GREEN}[*] TTL values adjusted successfully.${NC}"
 else
